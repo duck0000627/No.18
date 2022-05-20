f570bf04bd12cc6f950dcb752542f9bc7a98573a
Duck <40841131@gm.nfu.edu.tw>
2022-05-20 22:39:50 +0800

    sidebar

---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,41 @@
+<div style="margin-left:90px;margin-right: 700px">
+    <input class=" form-control me-2" type="search" placeholder="搜尋案件編號、員工姓名....." aria-label="Search" style="width: 240px">
+    <hr style="width: 250px ; border:1px solid">
+    <div class="row" style="width: 250px">
+        <div class="col fw-bold fs-3" style="text-align: center">案件</div>
+        <div class="col" style="text-align: right">
+            <button type="button" class="btn btn-link fs-3">+</button>
+        </div>
+    </div>
+    <hr style="width: 250px ; border:1px solid">
+    <table class="table" style="width: 240px">
+        <thead>
+        <tr>
+            <th scope="col">員工姓名</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">A001</th>
+        </tr>
+        <tr>
+            <th scope="row">A002</th>
+        </tr>
+        </tbody>
+    </table>
+    <table class="table" style="width: 240px">
+        <thead>
+        <tr>
+            <th scope="col">員工姓名</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <th scope="row">B001</th>
+        </tr>
+        <tr>
+            <th scope="row">B002</th>
+        </tr>
+        </tbody>
+    </table>
+</div>
