b54e0e93bf3aae4ef65ca9d0465f3dd72d4b04da
Duck <40841131@gm.nfu.edu.tw>
2022-05-22 20:23:17 +0800

    增加登出及字型大小的下拉框

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -8,8 +8,15 @@
             <i class="bi bi-x ml-20 cursor-pointer lg:hidden" onclick="Openbar()"></i>
         </div>
         <div>
-            <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
-                <i class="bi bi-gear"></i>
+            <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600"
+                 onclick="dropDown()">
+                <div class="flex justify-between w-full items-center">
+                    <i class="bi bi-gear"></i>
+                </div>
+            </div>
+            <div class=" leading-7 text-left text-sm font-thin mt-2 w-4/5 mx-auto" id="submenu">
+                <h1 class="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1">字型大小</h1>
+                <h1 class="cursor-pointer p-2 hover:bg-gray-700 rounded-md mt-1" onclick="logout()">登出</h1>
             </div>
             <div class="p-2.5 mt-2 flex items-center rounded-md px-4 duration-300 cursor-pointer  hover:bg-blue-600">
                 <i class="bi bi-list-check"></i>
@@ -30,9 +37,11 @@
         document.querySelector('#arrow').classList.toggle('rotate-0')
     }
 
-    dropDown()
-
     function Openbar() {
         document.querySelector('.sidebar').classList.toggle('left-[-300px]')
+    }
+
+    function logout() {
+        window.location.href = "{{route('login')}}";
     }
 </script>
